4956efcaf8f3153eec40751520675b6ffd65f506
Lorenzo Tellez <ltellez@gallopintonetworks.net>
2024-07-14 16:16:26 -0600

    Funcion Encriptar

--- script.js
+++ script.js
@@ -9,3 +9,48 @@ function actualizarPantalla(){
     
 
 }
+
+function encriptarMensaje() {
+    let mensaje = document.querySelector("#texto_usuario").value.trim();
+    let secreto = "";
+
+    if (esMensajeValido(mensaje)) {
+        secreto = encriptar(mensaje);
+        actualizarPantalla();
+        mostrar(secreto);
+        document.querySelector('#texto_usuario').value = '';
+    } else {
+        alert("Por favor, ingrese un mensaje en minúsculas y sin acentos");
+    }
+}
+
+function esMensajeValido(mensaje) {
+    return mensaje !== "" && !/[A-Zá-úÁ-Ú]/.test(mensaje) && mensaje.length > 0;
+}
+
+function encriptar(mensaje) {
+    let resultado = "";
+    for (let i = 0; i < mensaje.length; i++) {
+        switch (mensaje[i]) {
+            case "a":
+                resultado += "ai";
+                break;
+            case "e":
+                resultado += "enter";
+                break;
+            case "i":
+                resultado += "imes";
+                break;
+            case "o":
+                resultado += "ober";
+                break;
+            case "u":
+                resultado += "ufat";
+                break;
+            default:
+                resultado += mensaje[i];
+        }
+    }
+    return resultado;
+}
+
