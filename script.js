732c8b0636cf6c8ff2bab04110a89e841fd13e9d
Lorenzo Tellez <ltellez@gallopintonetworks.net>
2024-07-13 18:25:01 -0600

    Área para mostrar el texto encriptado/desencriptado.

---
+++ script.js
@@ -0,0 +1,11 @@
+function actualizarPantalla(){
+    
+    document.querySelector("#resultado").style.display="none";
+    document.querySelector("#muñeco").style.display="none";
+    document.querySelector("#mensaje_inicial").style.display="none";
+    document.querySelector("#mensaje_inicial_2").style.display="none";
+    document.querySelector("#texto_resultado").style.display="inline-block";
+    document.querySelector("#copiar").style.display="inline-block";
+    
+
+}
